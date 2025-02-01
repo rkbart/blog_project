5362e2630a63ef48ef70cf782a6422a0e0b4e980
rkbart <rk_bart@yahoo.com>
2025-02-01 21:58:04 +0800

    added show view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,12 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
+
+  def show
+    @post = Post.find(params[:id])
+  rescue ActiveRecord::RecordNotFound # error handling
+    redirect_to root_path # Redirect to the root path if the record is not found
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
+
+  resources :posts
+  get "/posts/:id" => "posts#show"
+
    root "posts#index"
 end
