92339d12b519f4061e2e7d960fb5f1e626d0815b
rkbart <rk_bart@yahoo.com>
2025-02-02 00:48:22 +0800

    added devise for authentication

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,8 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+user = User.where(email: "rk_bart@yahoo.com").first_or_initialize
+user.update!(
+    password: "password123",
+    password_confirmation: "password123"
+)
