b38c20b1d92bbeee33b2c13b0450e18df6cba7e6
rkbart <rk_bart@yahoo.com>
2025-02-03 03:15:02 +0800

    adjusted time zone

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,4 @@
 class Post < ApplicationRecord
-    has_many :comments, dependent: :destroy
+    has_many :comments, dependent: :destroy #realationship to comments. if post is deleted, comments are deleted
     validates :title, :body, presence: true
 end
--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,8 @@ module BlogProject
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.time_zone = 'Asia/Manila'
+    config.active_record.default_timezone = :local  # Store and display in local time
+
   end
 end
