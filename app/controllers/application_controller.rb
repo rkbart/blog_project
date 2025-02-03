5a198928acab92c96e5b345fd20e4fd1de13b4bc
rkbart <rk_bart@yahoo.com>
2025-02-04 00:09:52 +0800

    added login and sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  def after_sign_in_path_for(resource)
+    posts_path  # Redirects to posts index page
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   get "up" => "rails/health#show", as: :rails_health_check
+  
+  devise_scope :user do
+    root to: 'devise/sessions#new'  # Devise login page
+  end
+  
 
   resources :posts do #this line replaces all the routes below
     resources :comments, only: [:create, :destroy]
@@ -13,7 +18,7 @@ Rails.application.routes.draw do
   # delete "/posts/:id" => "posts#destroy"
   # post "/posts" => "posts#create" 
   
-  root "posts#index"
+  # root "posts#index"
 
 
 end
